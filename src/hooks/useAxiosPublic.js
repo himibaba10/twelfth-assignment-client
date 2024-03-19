0649b30ddbeba21bc8532da7fabf1dde1eb3e551
Ferdous Ahmed Himel <himibaba10@gmail.com>
2024-03-19 19:15:18 +0600

    Added the whole project to the public repo

--- src/hooks/useAxiosPublic.js
+++ src/hooks/useAxiosPublic.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://twelfth-assignment-server-ivory.vercel.app",
 });
 
 const useAxiosPublic = () => {
--- src/hooks/useAxiosSecure.js
+++ src/hooks/useAxiosSecure.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://twelfth-assignment-server-ivory.vercel.app",
 });
 
 const useAxiosSecure = () => {
